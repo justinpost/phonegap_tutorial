953f13b27475bbd4cdc19c91e17d7ffff8e614f3
justinpost <justin.m.post@gmail.com>
2014-01-21 07:34:58 +0100

    HomeView

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,4 +1,16 @@
 var HomeView = function(store) {
+	
+	this.render = function() {
+    	this.el.html(HomeView.template());
+    	return this;
+	};
+	
+	this.findByName = function() {
+		store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(HomeView.liTemplate(employees));
+		});
+	};
+	
 	this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
         this.el = $('<div/>');
--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 var app = {
 	
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
-    findByName: function() {
-		var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-	},
-	
 	showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -21,12 +9,11 @@ var app = {
 	},
 
     initialize: function() {
-		var self=this;
-        this.store = new WebSqlStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-    }
+		var self = this;
+		this.store = new MemoryStore(function() {
+			$('body').html(new HomeView(self.store).render().el);
+		});
+	}
 
 };
 
